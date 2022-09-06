1589a30ff63ab6bb45fc85055eff284c45f7f330
eeasyccode <81530450+eeasyccode@users.noreply.github.com>
2022-09-06 22:44:03 +0200

    color-changes

--- app/src/main/java/com/example/jobbe_application/MainActivity.kt
+++ app/src/main/java/com/example/jobbe_application/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.jobbe_application
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jobbe_application.ui.theme.JobbeApplicationTheme
+import com.example.jobbe_application.ui.theme.MainColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +23,9 @@ class MainActivity : ComponentActivity() {
             JobbeApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                    color = MaterialTheme.colors.surface,
+                    ) {
+
                 }
             }
         }
@@ -30,14 +33,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SplashScreenView(
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JobbeApplicationTheme {
-        Greeting("Android")
-    }
+) {
+
 }
--- app/src/main/java/com/example/jobbe_application/ui/theme/Color.kt
+++ app/src/main/java/com/example/jobbe_application/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.jobbe_application.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val Primary = Color(0xFFCD9F28)
+val PrimaryDark = Color(0xFF2B2B2B)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
